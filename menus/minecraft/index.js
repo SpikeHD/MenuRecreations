82891296dfc60026633bba4590ef105c47f8b2e7
SpikeHD <spikegdofficial@gmail.com>
2024-05-04 13:17:11 -0700

    feat: begin minecraft

---
+++ menus/minecraft/index.js
@@ -0,0 +1,10 @@
+const panos = {
+  main: {
+    file: 'assets/panorama_night.png',
+    src: 'https://www.planetminecraft.com/texture-pack/minecraft-cavesand-cliffs-part-ii-main-menu-panorama/'
+  },
+  old: {
+    file: 'assets/panorama.png',
+    src: 'https://www.deviantart.com/lockrikard/art/Minecraft-Alpha-Panorama-271986739'
+  }
+}
